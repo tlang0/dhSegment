c31ddec287aa1afb73339e25ca2c69157d52a489
Markus Diem <diemmarkus@gmail.com>
2018-06-11 15:10:16 +0200

    setup: packages added

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import setup, find_packages
 
 setup(name='dh_segment',
       version='0.1',
-      license='GPL',
       url='https://github.com/dhlab-epfl/dhSegment',
       description='Generic framework for historical document processing',
       packages=find_packages(exclude=['exps*']),
-      project_urls={
-          'Paper': 'https://arxiv.org/abs/1804.10371',
-          'Source Code': 'https://github.com/dhlab-epfl/dhSegment'
-      },
+      install_requires=[
+          'pillow', 'imageio', 'opencv-python', 'tqdm'
+      ],
       zip_safe=False)
