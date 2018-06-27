392a5e1aed800d2d000a8cafe348560ecee92108
Thomas Lang <tlang.dev@gmail.com>
2018-06-27 11:31:45 +0200

    better names, simpler overall calculation

--- eval.py
+++ eval.py
@@ -12,27 +12,26 @@ def eval_jaccard(filepath_result, filepath_gt):
 
     maxlabel = np.amax(img_gt)
     results = []
-    intersections_combined = np.zeros_like(img_gt)
+    intersection_area_overall = 0
     unions_overall = np.logical_or(np.not_equal(img_result, BGLABEL), np.not_equal(img_gt, BGLABEL))
     for label in range(maxlabel + 1):
         labelimg_gt = np.equal(label, img_gt)
         labelimg_result = np.equal(label, img_result)
         intersection = np.logical_and(labelimg_result, labelimg_gt)
-        count_intersection = np.count_nonzero(intersection)
-        union = np.logical_or(labelimg_result, labelimg_gt)
-        count_union = np.count_nonzero(union)
-        #count_union = np.count_nonzero(labelimg_gt) + np.count_nonzero(labelimg_result) - count_intersection
-        jaccard = count_intersection / count_union
+        intersection_area = np.count_nonzero(intersection)
+        #union = np.logical_or(labelimg_result, labelimg_gt)
+        #count_union = np.count_nonzero(union)
+        union_area = np.count_nonzero(labelimg_gt) + np.count_nonzero(labelimg_result) - intersection_area
+        jaccard = intersection_area / union_area
         # add tuple
-        results.append((label, jaccard, count_intersection, count_union))
+        results.append((label, jaccard, intersection_area, union_area))
         if label != BGLABEL: # ignore background for overall values
-            intersections_combined = np.logical_or(intersections_combined, intersection)
+            intersection_area_overall += intersection_area
 
-    count_intersections_combined = np.count_nonzero(intersections_combined)
-    count_unions_overall = np.count_nonzero(unions_overall)
-    jaccard_overall = count_intersections_combined / count_unions_overall
+    union_area_overall = np.count_nonzero(unions_overall)
+    jaccard_overall = intersection_area_overall / union_area_overall
 
-    return ((jaccard_overall, count_intersections_combined, count_unions_overall), results)
+    return ((jaccard_overall, intersection_area_overall, union_area_overall), results)
 
 def main():
     if len(sys.argv) == 3:
@@ -40,12 +39,12 @@ def main():
         if len(results) == 0:
             print("no results")
         else:
-            for (label, jaccard, count_intersection, count_union) in results:
+            for (label, jaccard, intersection_area, union_area) in results:
                 backgroundtext = " (bg)" if label == BGLABEL else ""
-                print("class {}{}: jaccard = {}, ({}/{})".format(label, backgroundtext, jaccard, count_intersection, count_union))
+                print("class {}{}: jaccard = {}, ({}/{})".format(label, backgroundtext, jaccard, intersection_area, union_area))
 
-            jaccard_overall, count_intersections_combined, count_unions_overall = result_overall
-            print("overall: jaccard = {}, ({}/{})".format(jaccard_overall, count_intersections_combined, count_unions_overall))
+            jaccard_overall, intersection_area_overall, union_area_overall = result_overall
+            print("overall: jaccard = {}, ({}/{})".format(jaccard_overall, intersection_area_overall, union_area_overall))
     else:
         print("usage:\n{} <filepath_result> <filepath_gt>".format(sys.argv[0]))
 
