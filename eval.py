6aa76e27117dcb672d6a14fe0aa0c7eef8ca8a84
Thomas Lang <tlang.dev@gmail.com>
2018-06-27 16:39:52 +0200

    consider num of regions
    * ignore non-existant labels
    * store num of regions
    * overall jaccard

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@
 import numpy as np
 import sys
 from imageio import imread
+import skimage.measure
 
 BGLABEL = 0
 
@@ -11,40 +12,39 @@ def eval_jaccard(filepath_result, filepath_gt):
     img_gt = imread(filepath_gt)
 
     maxlabel = np.amax(img_gt)
-    results = []
     intersection_area_overall = 0
     unions_overall = np.logical_or(np.not_equal(img_result, BGLABEL), np.not_equal(img_gt, BGLABEL))
+    results = []
     for label in range(maxlabel + 1):
+        if label == BGLABEL:
+            continue
         labelimg_gt = np.equal(label, img_gt)
+        gt_area = np.count_nonzero(labelimg_gt)
+        if gt_area <= 0:
+            continue
         labelimg_result = np.equal(label, img_result)
         intersection = np.logical_and(labelimg_result, labelimg_gt)
         intersection_area = np.count_nonzero(intersection)
-        #union = np.logical_or(labelimg_result, labelimg_gt)
-        #count_union = np.count_nonzero(union)
-        union_area = np.count_nonzero(labelimg_gt) + np.count_nonzero(labelimg_result) - intersection_area
+        union_area = gt_area + np.count_nonzero(labelimg_result) - intersection_area
         jaccard = intersection_area / union_area
-        # add tuple
-        results.append((label, jaccard, intersection_area, union_area))
-        if label != BGLABEL: # ignore background for overall values
-            intersection_area_overall += intersection_area
+        # count connected components in gt image
+        _, num_gt_regions = skimage.measure.label(labelimg_gt, return_num=True)
+        results.append((label, num_gt_regions, jaccard))
 
-    union_area_overall = np.count_nonzero(unions_overall)
-    jaccard_overall = intersection_area_overall / union_area_overall
+    results_mat = np.asarray(results, dtype=np.double)
+    # mean jaccard index weighted by number of components per class
+    jaccard_overall = np.dot(results_mat[:, 1], results_mat[:, 2]) / np.sum(results_mat[:, 1])
 
-    return ((jaccard_overall, intersection_area_overall, union_area_overall), results)
+    return jaccard_overall, results_mat
 
 def main():
     if len(sys.argv) == 3:
-        result_overall, results = eval_jaccard(sys.argv[1], sys.argv[2])
-        if len(results) == 0:
+        jaccard_overall, results_mat = eval_jaccard(sys.argv[1], sys.argv[2])
+        if len(results_mat) == 0:
             print("no results")
         else:
-            for (label, jaccard, intersection_area, union_area) in results:
-                backgroundtext = " (bg)" if label == BGLABEL else ""
-                print("class {}{}: jaccard = {}, ({}/{})".format(label, backgroundtext, jaccard, intersection_area, union_area))
-
-            jaccard_overall, intersection_area_overall, union_area_overall = result_overall
-            print("overall: jaccard = {}, ({}/{})".format(jaccard_overall, intersection_area_overall, union_area_overall))
+            print(jaccard_overall)
+            print(results_mat)
     else:
         print("usage:\n{} <filepath_result> <filepath_gt>".format(sys.argv[0]))
 
